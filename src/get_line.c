a15ba1a546f18f1f1e5523cc14f7d4e9ae6e3f95
phz <phz@example.com>
2023-09-25 17:24:32 -0300

    fix: don't add empty line to history

--- src/get_line.c
+++ src/get_line.c
@@ -6,7 +6,7 @@
 /*   By: dmanoel- <dmanoel-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 18:52:51 by mflavio-          #+#    #+#             */
-/*   Updated: 2023/08/30 23:34:50 by dmanoel-         ###   ########.fr       */
+/*   Updated: 2023/09/25 17:23:12 by dmanoel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ char	*get_line(void)
 	char	*line;
 
 	line = readline("$> ");
-	add_history(line);
+	if (line && ft_strlen(line))
+		add_history(line);
 	return (line);
 }
